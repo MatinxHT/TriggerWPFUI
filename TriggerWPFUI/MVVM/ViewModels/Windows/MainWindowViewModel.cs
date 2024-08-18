0c8716b5ee5f51211f8206c615866f1d8202f11f
Chiho Hung <linduanpeng@live.com>
2024-08-18 20:36:55 +0800

    Add project files.

---
+++ TriggerWPFUI/MVVM/ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriggerWPFUI.MVVM.Views;
+using Wpf.Ui.Controls;
+
+namespace TriggerWPFUI.MVVM.ViewModels.Windows
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        [ObservableProperty]
+        private string _appcationTitle = "Trigger Demo of WPF-UI";
+
+        [ObservableProperty]
+        private ObservableCollection<object> _menuItems = [
+            new NavigationViewItem("Home", SymbolRegular.Home12,typeof(BlankPage)),
+            new NavigationViewItemSeparator(),
+            new NavigationViewItem()
+            {
+                Content = "CollectionA",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.Radar20 },
+                MenuItemsSource = new object[]
+                {
+                    new NavigationViewItem("ItemA", SymbolRegular.TextFont24, typeof(BlankPage)),
+                    new NavigationViewItem("ItemB", SymbolRegular.Color24, typeof(BlankPage))
+                }
+            }
+            ];
+
+        [ObservableProperty]
+        private ObservableCollection<object> _footerMenuItems =
+            [
+            new NavigationViewItem("Settings and About", SymbolRegular.Settings24, typeof(BlankPage))
+            ];
+    }
+}
---
+++ TriggerWPFUI/MVVM/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TriggerWPFUI.MVVM.ViewModels.Windows;
+
+namespace TriggerWPFUI.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowViewModel ViewModel = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+        }
+    }
+}
